5aa9c05cbb64db024630598e9bfde15c6f5ccc22
Henry <jm25002@esfe.agape.edu.sv>
2025-05-25 12:25:33 -0600

    Se creo la clase y se realizaron pruebas

--- FrutasInversas.java
+++ FrutasInversas.java
@@ -1,3 +1,22 @@
-public class FrutasInversas {
+import java.util.Scanner;
 
+public class FrutasInversas {
+public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] frutas = new String[10];
+
+        // Guardar las 10 frutas
+        for (int i = 0; i < frutas.length; i++) {
+            System.out.print("Ingrese el nombre de la fruta #" + (i + 1) + ": ");
+            frutas[i] = scanner.nextLine();
+        }
+
+        // Mostrar las frutas en orden inverso
+        System.out.println("\n--- Frutas en orden inverso ---");
+        for (int i = frutas.length - 1; i >= 0; i--) {
+            System.out.println(frutas[i]);
+        }
+
+        scanner.close();
+    }
 }
