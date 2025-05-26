20a93461ba26020b4bb7b6da16bf1120bdf31750
Henry <jm25002@esfe.agape.edu.sv>
2025-05-26 00:01:36 -0600

    Se creo la clase "automovil"

---
+++ EjercicioVehiculo/Automovil.java
@@ -0,0 +1,8 @@
+package EjercicioVehiculo;
+
+public class Automovil extends Vehiculo {
+    @Override
+    public void mover() {
+        System.out.println("El automóvil está rodando sobre ruedas.");
+    }
+}
